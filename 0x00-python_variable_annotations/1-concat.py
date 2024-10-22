cd71ddd325779a9f876ba3d3c6562c0a5d5e6f56
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-22 15:51:22 +0300

    1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """that takes a string str1 and a string str2"""
+    return (str1 + str2)
