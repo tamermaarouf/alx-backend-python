6cdef14f8bfa0011ecf3552b926dbe1d62c142e5
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-22 17:02:58 +0300

    4. Define variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-#Define and annotate the following variables with the specified values:
+"""Define and annotate the following variables with the specified values:"""
 
 
-a: int = 1                                  # integer with a value of 1
-pi: float = 3.14                            # float with a value of 3.14
-i_understand_annotations: bool = True       # a boolean with a value of True
-school: str = 'Holberton'                   # a string with a value of 'Holberton'
+a: int = 1                                  """integer with a value of 1"""
+pi: float = 3.14                            """float with a value of 3.14"""
+i_understand_annotations: bool = True       """boolean with a value of True"""
+school: str = 'Holberton'                   """string with a value """
